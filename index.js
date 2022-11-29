57d5ce8756073215b0f7dacdaaa6953f467fba58
regular-meet-8 <eddiekim444@gmail.com>
2022-11-29 10:42:49 +0300

    done js2 array-lab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+let cats = [`Milo`, `Otis`, `Garfield`]
+
+function destructivelyAppendCat(name =`Ralph`){
+
+    return cats.push(name)
+}
+function destructivelyPrependCat(name = `Bob`){
+    return cats.unshift(name)
+}
+function destructivelyRemoveLastCat(){
+    return cats.pop(`Garfield`)
+}
+function destructivelyRemoveFirstCat(){
+    return cats.shift(`Milo`)
+}
+function appendCat(){
+const appendCat = [...cats, `Broom`]
+return appendCat
+
+}
+function prependCat(){
+    const prependCat = [`Arnold`, ...cats]
+    return prependCat
+}
+function removeLastCat(){
+const removeLastCat = cats.slice(0,2)
+return removeLastCat
+
+}
+function removeFirstCat(){
+    const removeFirstCat = cats.slice(1)
+    return removeFirstCat
+}
+
+
+
+
+
+
+
